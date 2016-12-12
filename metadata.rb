026e2e765a1bdf310e243d9c0564f94330be7c69
Simran pal <simranp@thoughtworks.com>
2016-12-12 14:34:45 +0530

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'set_proxy'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures set_proxy'
+long_description 'Installs/Configures set_proxy'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/set_proxy/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/set_proxy' if respond_to?(:source_url)
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: set_proxy
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
